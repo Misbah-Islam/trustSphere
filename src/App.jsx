d7310332718a98225b5b7b810954ed546ae8ae16
Misbah Islam <225596806+Misbah-Islam@users.noreply.github.com>
2026-04-23 03:37:27 +0500

    fix: use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './context';
 import { Navbar } from './components';
 import { useApp } from './context';
